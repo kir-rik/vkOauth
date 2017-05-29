8259f323656332f5e13201b0c67ddf20a98b5d79
k.ponomarenko <k.ponomarenko@PONOMARENKO>
2017-05-29 17:50:30 +0400

    key

--- app/vkApiService.js
+++ app/vkApiService.js
@@ -1,7 +1,6 @@
-// var fetch = require('node-fetch');
 var axios = require('axios');
 const app_id = 6046074;
-const secretKey = 'iqzs9KFLo7vQXe4phzsM';
+const secretKey = 'iqzs9KFLo7vQXe4phzsM'; //<-- PUT ACTUAL KEY HERE
 
 function getAccessToken(code){
 
