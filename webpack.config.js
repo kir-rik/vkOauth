5f3ca6c5838e31212b108365da334579517f8bfc
k.ponomarenko <k.ponomarenko@PONOMARENKO>
2017-06-01 19:04:29 +0400

    some stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,49 @@
+'use strict'
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
+
+    // entry: {
+    //     home: './frontend'
+    // }, 
     entry: './index.js',
+
     output: {
         path: __dirname,
         filename: 'dist/bundle.js',
     },
+
+    //watch: NODE_ENV == 'development',
+
+    // watchOptions: {
+    //     aggregateTimeouit: 100
+    // },
+
+    devtool: NODE_ENV == 'development' ? 'source-map' : null,
+
     node: {
         net: 'empty',
         fs: 'empty',
     },
-    // module: {
-    //     loaders: [
-    //         { test: /\.css$/, loader: 'style!css' },
-    //     ],
-    // },
+    plugins: [
+        //new webpack.DefinePlugin({NODE_ENV: JSON.stringify(NODE_ENV)})
+    ],
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: 'style!css' },
+        ],
+    },
 };
+
+
+// module.exports.plugins.push(
+//     new webpack.optimize.UglifyJsPlugin({
+//         compress: {
+//             warnings: false,
+//             drop_console: false,
+//             unsafe: false
+//         }
+//     })
+// );
